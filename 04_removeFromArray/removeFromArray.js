39c11f75672232f4e0a0869d02eb6eeb791b0088
Katelyn <kcm34@pitt.edu>
2023-04-01 09:28:38 -0700

    Changes made to 04 Javascript exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
 
-};
+  const newArray = [];
 
+  for (i=0; i<array.length; i++) {
+    if (!args.includes(array[i])) {
+        newArray.push(array[i]);
+    }
+  }
+ 
+//   function itempush(item) {
+//     if (!args.includes(item)) {
+//         newArray.push(item);
+//     }
+//  }
+
+//    array.forEach(itempush);
+
+
+   return newArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
