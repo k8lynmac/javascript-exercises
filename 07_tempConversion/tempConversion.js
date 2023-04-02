607088a09b3301ed173edc7a3170d28c510345d6
Katelyn <kcm34@pitt.edu>
2023-04-02 14:55:44 -0700

    Changes made to javascript files to pass tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
-};
+const convertToCelsius = function(fDegree) {
+let celsius = (fDegree-32)/1.8;
+if (celsius == 0) {
+  return 0;
+} else {
+let result = parseFloat(celsius.toFixed(1));
+return result;
+}
+}
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cDegree) {
+  let fahrenheit=(cDegree*1.8)+32;
+  let result = parseFloat(fahrenheit.toFixed(1));
+  return result;
 };
 
 // Do not edit below this line
