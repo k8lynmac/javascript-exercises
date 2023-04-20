06166b03d64013a669a6079b8dae6c432254373c
Katelyn <kcm34@pitt.edu>
2023-04-19 21:01:38 -0700

    solution to palindrome exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let small = string.replace(/\W/g, "").toLowerCase();
+    let len = filtered.length;
+    
+    if (len % 2 === 0) {
+        for (let i = 0; i < len - i - 1; i++) {
+            if (filtered[i] !== filtered[len - i -1]) {
+                return false;
+            }
+        } 
+        return true;
+    } 
+    else {
+        for (let i = 0; i <= len - i - 1; i++) {
+            if (filtered[i] !== filtered[len - i -1]) {
+                return false;
+            }
+        } 
+        return true;
+    }
 };
+
+// const palindromes = function (string) {
+//     const processedString = string.toLowerCase().replace(/[^a-z]/g, "");
+//     return processedString.split("").reverse().join("") == processedString;
+//   };
 
 // Do not edit below this line
 module.exports = palindromes;
