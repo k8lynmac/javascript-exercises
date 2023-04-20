43388a141d1c66d147cd1deb4eddc982c2b0f452
Katelyn <kcm34@pitt.edu>
2023-04-19 21:00:39 -0700

    solution for calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let end = 0;
+	for (let i=0; i<array.length; i++) {
+    end += array[i];
+  }
+  return end;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let end = 1;
+	for (let i=0; i<array.length; i++) {
+    end = end * array[i];
+  }
+  return end;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let fact=1;
+  for(let i=0; i < a; i++) {
+    fact = fact*(a-i);
+    if (a === 0) {
+      fact = 1;
+      return fact;
+    }
+  }
+  return fact;
 };
 
 // Do not edit below this line
