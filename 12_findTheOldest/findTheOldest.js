edfd474c5d00eb00712ff02caf888fc4cf5a1b90
Katelyn <kcm34@pitt.edu>
2023-04-21 20:12:35 -0700

    working function to sort array

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    //finds property in each object of array and returns existing year if empty
+    for (let key in people) {
+        if (people[key].yearOfDeath === undefined) {
+            people[key].yearOfDeath = new Date().getFullYear();
+        }
+    }
+
+    console.table(people);
+
+    //function that sorts objects by years lived with oldest at people[0]
+   people.sort(function(a, b) {
+        const person1 = a.yearOfDeath - a.yearOfBirth;
+        const person2 = b.yearOfDeath - b.yearOfBirth;
+        //if <0, sort a before b
+        return person1 > person2 ? -1 : 1;
+      });
+
+      const oldest = people[0];
+      return oldest;
 };
 
 // Do not edit below this line
